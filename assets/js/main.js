4f75cb1e9598c9a5243decc6f6d7d963493266f7
yichu <yichu.yang@gmail.com>
2021-02-21 23:49:44 -0500

    version 1.15 mobile compatibility test

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,12 +62,20 @@ jQuery(document).ready(function($) {
         }
     );
     
+	$('.navbar-nav>li>a').on('click', function(){
+	    $('.navbar-collapse').collapse('hide');
+	});
+	
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+	
+		$('.navbar-nav>li>a').on('click', function(){
+		    $('.navbar-collapse').collapse('hide');
+		});
 
 
 });
